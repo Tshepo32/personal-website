539c3b637e54e0f1f12d6bf3c6f758fec6866230
Tshepo <221093990@mycput.ac.za>
2025-05-17 22:05:37 +0200

    Backend

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from 'react';
+import './Navbar.css';
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+    const closeMenu = () => setIsOpen(false);
+
     return (
         <nav className="navbar">
-            <h1>Tshepo Maleo</h1>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+            <div className="navbar-brand">
+                <h1>Tshepo Maleo</h1>
+                <div className="hamburger" onClick={toggleMenu}>
+                    &#9776;
+                </div>
+            </div>
+            <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+                <li><a href="#home" onClick={closeMenu}>Home</a></li>
+                <li><a href="#about" onClick={closeMenu}>About</a></li>
+                <li><a href="#projects" onClick={closeMenu}>Projects</a></li>
+                <li><a href="#certifications" onClick={closeMenu}>Certifications</a></li>
+                <li><a href="#contact" onClick={closeMenu}>Contact</a></li>
             </ul>
         </nav>
     );
