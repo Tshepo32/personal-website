4b0225bb1ea55c5403fec9acfc03da407d8ebcc8
Tshepo <221093990@mycput.ac.za>
2025-05-23 14:39:34 +0200

    connect front and back

--- src/Home.js
+++ src/Home.js
@@ -29,7 +29,7 @@ function Home() {
     }, [location]);
 
     useEffect(() => {
-        fetch('http://localhost:8080/api/blog/getall?limit=2')
+        fetch('http://personal-website-16.onrender.com/api/blog/getall?limit=2')
             .then(response => {
                 if (!response.ok) throw new Error('Network response was not ok');
                 return response.json();
